211ef0677b9fb3eaf83f983d37a3750b8d1908a9
Gabriel Marinho <g.marinho@dromeda.org>
2024-04-16 16:41:25 +0000

    adds CV to download for recruiters

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
                   <nav className="ml-auto text-sm font-medium space-x-6 mr-5">
                     <Link href="/">Home</Link>
                     <Link href="/about">About</Link>
+                    <Link href="/CV_GabrielMarinho.pdf">CV</Link>
                   </nav>
                   <ModeToggle />
                 </div>
